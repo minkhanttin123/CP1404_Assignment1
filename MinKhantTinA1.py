afb656e2ddfc6f1ebc923057c45f95107750c36d
Min Khant Tin <woodencapo.mkt@gmail.com>
2016-09-02 16:14:49 +0800

    introduction, welcoming message and show menu
    Start with introduction including my name, date , brief summary of the program and git hub account link.

---
+++ MinKhantTinA1.py
@@ -0,0 +1,19 @@
+'''Min Khant Tin
+    1/9/1016
+    This a shopping list program which you can add, list, mark and see the items you need to shop
+    https://github.com/minkhanttin123/CP1404_Assignment1'''
+
+
+#This is going to welcome the user and show the title
+print("Welcome to the Shopping List 1.0 by Min Khant Tin")
+
+#This is going to show the menu
+def show_menu():
+    menu = '''
+Menu:
+R - List required items
+C - List completed items
+A - Add new item
+M - Mark an item as completed
+Q - Quit'''
+    print(menu)
